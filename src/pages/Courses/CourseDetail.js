10387996db38639ed292ee5cd3ba9e242e26ffcb
Obaidul hasan <md.obaidulhasnlimu@gmaiol.comgit config --global user.name Obaidul>
2022-10-27 02:22:34 +0600

    design

--- src/pages/Courses/CourseDetail.js
+++ src/pages/Courses/CourseDetail.js
@@ -6,32 +6,14 @@ const CourseDetail = () => {
     const data = useLoaderData();
     console.log(data);
     return (
-        // <div className='w-11/12 mx-auto grid grid-cols-1 lg:grid-cols-3 gap-3'>
-        //     {
-        //         Courses.map(item => <div key={item.id} className="card bg-base-100 shadow-xl">
-        //             <figure><img src={item?.img} alt="" /></figure>
-        //             <div className="card-body">
-        //                 <h2 className="card-title">
-        //                     {item?.name}
-        //                 </h2>
-        //                 <div className="card-actions justify-between">
-        //                     <div className="badge badge-outline">Lectures:{item?.lectures}</div>
-        //                     <div className="badge badge-outline">Quiz: {item?.quiz}</div>
-        //                 </div>
-        //             </div>
-        //         </div>)
-        //     }
-
-
-
-        // </div>
+        
         <div>
             <h1>welcome to Dev-Ex </h1>
             <img src="https://ibb.co/K22nZRQ" alt=""/>
             <div>
             <div className="card-body">
             
-                         <h2 className="card-title">
+                         <h2 className=" bg-pink-300 card-title">
                             {data?.name}
                             
                          </h2>
@@ -39,19 +21,19 @@ const CourseDetail = () => {
                             <div className="card bg-base-100 shadow-xl">
                             <figure><img src={data?.img} alt="" /></figure>
                             </div>
-                            <div className="badge badge-outline">Details:{data?.details}</div>
-                            <div className="badge badge-outline">Price:{data?.price}</div>
-                            <div className="badge badge-outline">Lectures:{data?.lectures}</div>
-                            <div className="badge badge-outline">Quiz: {data?.quiz}</div>
+                            <div className=' bg-sky-200'>Details:{data?.details}</div>
+                            <div className=" bg-green-300 badge badge-outline">Price:{data?.price}</div>
+                            <div className="  bg-orange-300 badge badge-outline">Lectures:{data?.lectures}</div>
+                            <div className="bg-blue-500 badge badge-outline">Quiz: {data?.quiz}</div>
                         </div>
                      </div>
             </div>
-            {data?.name}
+            {/* {data?.name}
             {data?.img}
             {data?.details}
             {data?.price}
             {data?.lectures}
-            {data?.quiz}
+            {data?.quiz} */}
 
 
         </div>
