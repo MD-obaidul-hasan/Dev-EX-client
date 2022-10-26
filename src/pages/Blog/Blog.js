c7c7f29a0528f5a44a5e89c0ec01290963d5379d
Obaidul hasan <md.obaidulhasnlimu@gmaiol.comgit config --global user.name Obaidul>
2022-10-27 03:34:23 +0600

    blog

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -2,8 +2,31 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-            blog
+        <div class="grid grid-rows-4 grid-flow-col gap-4">
+           <div className=" card w-96 bg-base-100 shadow-xl">
+  <figure className="px-10 pt-10">
+    <img src="https://avatars.githubusercontent.com/u/25158?s=280&v=4" alt="Shoes" className="rounded-xl" />
+  </figure>
+  <div className="card-body items-center text-center">
+    <h2 className="card-title">PHP</h2>
+    <p>PHP is a server side scripting language that is embedded in HTML. It is used to manage dynamic content, databases, session tracking, even build entire e-commerce sites. It is integrated with a number of popular databases, including MySQL, PostgreSQL, Oracle, Sybase, Informix, and Microsoft SQL Server.?</p>
+    <div className="card-actions">
+      {/* <button className="btn btn-primary">Buy Now</button> */}
+    </div>
+  </div>
+</div>
+<div className="card w-96 bg-base-100 shadow-xl">
+  <figure className="px-10 pt-10">
+    <img src="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/130147272/original/f771598bfb790f953b81a36b17692a167dd62fb6/do-asp-dot-net-core-and-angular-web-application.jpg" alt="Shoes" className="rounded-xl" />
+  </figure>
+  <div className="card-body items-center text-center">
+    <h2 className="card-title">ASP.NET</h2>
+    <p>NET (dot NET) course is a software developing course, where the developers use dot Net framework and machine level languages to develop different software. It is a free open source platform to develop different types of software applications."?</p>
+    <div className="card-actions">
+      {/* <button className="btn btn-primary">Buy Now</button> */}
+    </div>
+  </div>
+</div>
         </div>
     );
 };
